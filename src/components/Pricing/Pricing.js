9926e9d496dcff52c10e820460b0a6a93c171e4d
shagors <shajjadshagor@gmil.com>
2022-03-28 10:01:31 +0600

    humburg menu add

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PricingOption from '../PricingOption/PricingOption';
 
 const Pricing = () => {
-    const pricing = [
+    const pricingOptions = [
         {id: 1, name: 'Free', price: 0},
         {id: 2, name: 'Regular', price: 9.99},
         {id: 1, name: 'Premium', price: 19.99},
@@ -10,8 +11,10 @@ const Pricing = () => {
         <div className='bg-indigo-300 p-4 mt-8'>
             <h1 className='text-6xl font-mono text-white'>Best Deals of the town</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt dolor ducimus deleniti saepe quo recusandae, quis officia aliquam eum qui temporibus asperiores sunt ipsa possimus dolore, libero veniam cum! Magni?</p>
-            <div>
-                
+            <div className='grid md:grid-cols-3 gap-3 mt-8'>
+                {
+                    pricingOptions.map(option => <PricingOption key={option.id} option={option}></PricingOption>)
+                }
             </div>
         </div>
     );
--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const PricingOption = () => {
+const PricingOption = (props) => {
+    const {name, price} = props.option;
     return (
-        <div>
-            
+        <div className='bg-white p-4 rounded-lg'>
+            <h2 className='bg-indigo-300 py-2 rounded text-xl font-bold'>{name}</h2>
+            <p>
+                <span className='text-5xl font-bold'>{price}</span>
+                <span className='text-xl font-bold text-gray-500'>/mo</span>
+            </p>
         </div>
     );
 };
