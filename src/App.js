1453339c1caed009bc3aa70f51d3f4c2e4dbdaa1
shagors <shajjadshagor@gmil.com>
2022-03-28 09:36:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Pricing from './components/Pricing/Pricing';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <h1 className='text-6xl'>Welcome to my pricing club</h1>
+      <Pricing></Pricing>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Pricing = () => {
+    const pricing = [
+        {id: 1, name: 'Free', price: 0},
+        {id: 2, name: 'Regular', price: 9.99},
+        {id: 1, name: 'Premium', price: 19.99},
+    ]
+    return (
+        <div className='bg-indigo-300 p-4 mt-8'>
+            <h1 className='text-6xl font-mono text-white'>Best Deals of the town</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt dolor ducimus deleniti saepe quo recusandae, quis officia aliquam eum qui temporibus asperiores sunt ipsa possimus dolore, libero veniam cum! Magni?</p>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Pricing;
